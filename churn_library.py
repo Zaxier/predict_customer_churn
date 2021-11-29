6d187f18e672ec75c7e9d6556558dcf40b3f699a
Xavier Armitage <xtarmitage@gmail.com>
2021-11-29 21:23:35 +1100

    first draft finished

--- churn_library.py
+++ churn_library.py
@@ -1,8 +1,21 @@
-# library doc string
+'''
+Docstring here
+'''
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import joblib
+import shap
+from PIL import Image, ImageDraw
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import plot_roc_curve, classification_report
 
 def import_data(pth):
     '''
@@ -56,6 +69,7 @@ def perform_eda(df):
     sns.distplot(df['Total_Trans_Ct'])
     plt.savefig('images/eda/total_transaction_distribution.png')
 
+
 def encoder_helper(df, category_lst, response):
     '''
     helper function to turn each categorical column into a new column with
@@ -77,7 +91,7 @@ def encoder_helper(df, category_lst, response):
     return df
 
 
-def perform_feature_engineering(df, response):
+def perform_feature_engineering(df, response=None):
     '''
     input:
               df: pandas dataframe
@@ -89,6 +103,21 @@ def perform_feature_engineering(df, response):
               y_train: y training data
               y_test: y testing data
     '''
+    y = df.Churn
+    X = pd.DataFrame()
+    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
+                 'Total_Relationship_Count', 'Months_Inactive_12_mon',
+                 'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
+                 'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
+                 'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
+                 'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn',
+                 'Income_Category_Churn', 'Card_Category_Churn']
+
+    X[keep_cols] = df[keep_cols]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    return X_train, X_test, y_train, y_test
 
 
 def classification_report_image(y_train,
@@ -111,7 +140,18 @@ def classification_report_image(y_train,
     output:
              None
     '''
-    pass
+    rf_test_classification_report = classification_report(y_test, y_test_preds_rf)
+    lr_test_classification_report = classification_report(y_test, y_test_preds_lr)
+
+    report_lst = [rf_test_classification_report, lr_test_classification_report]
+    name_lst = ['rf_results', 'logistic_results']
+
+    # generate image of results for each model
+    for report, name in zip(report_lst, name_lst):
+        img = Image.new('RGB', (400, 200), color='white')
+        draw = ImageDraw.Draw(img)
+        draw.text((10, 10), report, fill='black')
+        img.save(f'images/results/{name}.png')
 
 
 def feature_importance_plot(model, X_data, output_pth):
@@ -139,7 +179,53 @@ def train_models(X_train, X_test, y_train, y_test):
     output:
               None
     '''
-    pass
+
+    # grid search
+    rfc = RandomForestClassifier(random_state=42)
+    lrc = LogisticRegression()
+
+    # param_grid = {
+    #     'n_estimators': [200, 500],
+    #     'max_features': ['auto', 'sqrt'],
+    #     'max_depth': [4, 5, 100],
+    #     'criterion': ['gini', 'entropy']
+    # }
+
+    param_grid = {
+        'n_estimators': [20, 50],
+        'max_features': ['auto', 'sqrt'],
+        'max_depth': [4],
+        'criterion': ['gini']
+    }
+
+    cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+    cv_rfc.fit(X_train, y_train)
+
+    lrc.fit(X_train, y_train)
+
+    y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train)
+    y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test)
+
+    y_train_preds_lr = lrc.predict(X_train)
+    y_test_preds_lr = lrc.predict(X_test)
+
+    classification_report_image(
+        y_train, y_test,
+        y_train_preds_lr, y_train_preds_rf,
+        y_test_preds_lr, y_test_preds_rf
+    )
+
+    feature_importance_plot(
+        cv_rfc.best_estimator_,
+        X_test,
+        'images/results/feature_importances.png')
+
+    # save best model
+    joblib.dump(cv_rfc.best_estimator_, './models/rfc_model.pkl')
+    joblib.dump(lrc, './models/logistic_model.pkl')
+
+
+
 
 if __name__ == "__main__":
     df = import_data('./data/bank_data.csv')
